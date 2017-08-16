eeee6bda3a13dd7dbb0b351ec46294d0aab498c0
Mitchell Long and LDMacKrell <mitchell@>
2017-08-16 11:47:38 -0700

    add user interface logic for collecting input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
-$(document).ready(function(event){
-  event.preventDefault();
-  
-})
+// Business Logic
+var translator = function(english){
+  return false;
+}
+
+
+
+
+
+// User Interface
+$(document).ready(function(){
+  $("form#userInput").submit(function(event){
+      event.preventDefault();
+      var sentence = $("#inputSentence").val();
+      var result = translator(sentence);
+      console.log(result);
+
+  });
+});
