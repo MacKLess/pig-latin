7e1a5e7beeed78fea324d50f99e9072978ce6ed7
Mitchell Long and LDMacKrell <mitchell@>
2017-08-16 13:57:52 -0700

    add arrays for vowels and consonants to the business logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
 // Business Logic
+var vowels = ["a", "e", "i", "o", "u"]
+var consonents = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+
+
+
 var translator = function(english){
-  return false;
 }
 
+var findSpaces = function(letter){
+  for (i=0; i<=letter.length; i++){
+    if (letter[i] === " "){
+      collector.push(i)
+    }
+  }
+}
 
-
+var collector = []
 
 
 // User Interface
@@ -12,8 +23,12 @@ $(document).ready(function(){
   $("form#userInput").submit(function(event){
       event.preventDefault();
       var sentence = $("#inputSentence").val();
-      var result = translator(sentence);
-      console.log(result);
+      var sentenceArray = sentence.split("")
+      var result = translator(sentenceArray);
+      console.log(sentenceArray);
+      // console.log(result);
+      var spaceIndex = findSpaces(sentenceArray);
+      console.log(collector);
 
   });
 });
