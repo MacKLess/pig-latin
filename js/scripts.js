7364fcca38f76cab07c22f1feb9a617e26a70cca
Mitchell Long and LDMacKrell <mitchell@>
2017-08-16 14:39:15 -0700

    added first letter identifier buisness logic.

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,46 @@
 var vowels = ["a", "e", "i", "o", "u"]
 var consonents = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
 
+var isVowel = function(currentLetter){
+  for (i =0; i < currentLetter.length; i++){
+    if (vowels[i] === sentenceArray[0]){
+      return true;
+    }
+  }
+}
+var isConsonent = function(currentLetter){
+  for (i = 0; i < currentLetter.length; i++){
+    if (consonents[i] === sentenceArray[0]){
+      return true;
+    }
+  }
+}
+if (isVowel === true){
+  var test = sentenceArray.push("way")
+  console.log(test);
+}
+//
+//
+// if (sentenceArray[0] === vowels || sentenceArray[0] === consonents)
+//
+// var startConsonent = function(){
+//   for ()
+// }
+
 
 
 var translator = function(english){
 }
 
-var findSpaces = function(letter){
-  for (i=0; i<=letter.length; i++){
-    if (letter[i] === " "){
-      collector.push(i)
-    }
-  }
-}
-
-var collector = []
+// var findSpaces = function(letter){
+//   for (i=0; i<=letter.length; i++){
+//     if (letter[i] === " "){
+//       collector.push(i)
+//     }
+//   }
+// }
+//
+// var collector = []
 
 
 // User Interface
@@ -25,10 +51,13 @@ $(document).ready(function(){
       var sentence = $("#inputSentence").val();
       var sentenceArray = sentence.split("")
       var result = translator(sentenceArray);
-      console.log(sentenceArray);
+      // console.log(sentenceArray);
       // console.log(result);
-      var spaceIndex = findSpaces(sentenceArray);
-      console.log(collector);
 
+
+
+
+
+      // var spaceIndex = findSpaces(sentenceArray);
   });
 });
